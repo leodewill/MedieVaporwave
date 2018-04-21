3a231f697304178c7592298310a2a8b4badf2826
Unknown <will.leidan@gmail.com>
2018-04-21 12:12:40 -0300

    Raycaster Basics

---
+++ Assets/Scripts/Clickable.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clickable : MonoBehaviour {
+
+    public virtual void OnPointerEnter() { }
+    public virtual void OnPointerExit() { }
+    public virtual void OnClick(int mouseButton) { }
+    public virtual void OnStartDrag() { }
+    public virtual void OnDrag() { }
+    public virtual void OnEndDrag() { }
+}
---
+++ Assets/Scripts/Raycaster.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycaster : MonoBehaviour {
+    
+    Clickable clickable;
+
+    void Awake() {
+        clickable = null;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetAxisRaw("Mouse X") != 0 || Input.GetAxisRaw("Mouse Y") != 0) {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 20.0f)) {
+                Clickable c = hit.transform.gameObject.GetComponent<Clickable>();
+                if (c != null) {
+                    Hit(c);
+                } else
+                    NoHit();
+            } else {
+                NoHit();
+            }
+        }
+
+        if (clickable != null) {
+            if (Input.GetMouseButtonDown(0)) {
+                clickable.OnClick(0);
+            }
+            if (Input.GetMouseButtonDown(1)) {
+                clickable.OnClick(1);
+            }
+
+        }
+    }
+
+    void Hit(Clickable hit) {
+        if (clickable != hit) {
+            if (clickable != null) {
+                clickable.OnPointerExit();
+            }
+            clickable = hit;
+            clickable.OnPointerEnter();
+        }
+    }
+
+    void NoHit() {
+        if (clickable != null) {
+            clickable.OnPointerExit();
+            clickable = null;
+        }
+    }
+}
