78aeaa2480c2ae28aeb6221d54357d17b7098ce1
Unknown <will.leidan@gmail.com>
2018-04-21 15:33:44 -0300

    Mouse Look Camera

---
+++ Assets/Scripts/Managers/CameraControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+    public GameObject target;
+    Vector3 destiny;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        destiny = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - this.transform.position).normalized * 100.0f;
+        target.transform.localPosition = Vector3.Lerp(target.transform.position, destiny, 0.2f);
+	}
+}
