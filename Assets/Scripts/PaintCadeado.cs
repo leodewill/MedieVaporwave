86639ee16dccb297e515c10d2ceeb55dfaa57c26
Unknown <will.leidan@gmail.com>
2018-04-23 17:02:01 -0300

    Moldura Animations

---
+++ Assets/Scripts/PaintCadeado.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaintCadeado : MonoBehaviour {
+
+    bool up = false;
+    public float speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (up) {
+            this.transform.parent.position += speed * Time.deltaTime * Vector3.up;
+        }
+	}
+
+    public void fly() {
+        up = true;
+        Invoke("Die", 4.0f);
+    }
+
+    void Die() {
+        Destroy(this.transform.parent.gameObject);
+    }
+}
